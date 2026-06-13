56b225fc37af0ba8629cc8a9aa9e243254c87c6b
amilizikri54-ship-it <amilizikri54@gmail.com>
2026-06-14 06:39:52 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -12,7 +12,7 @@ export const botConfig = {
   // - "invisible" = appears offline
   presence: {
     // Current online state shown on Discord.
-    status: "online",
+    status: "dnd",
 
     // Activity lines shown under the bot name.
     // `type` number mapping from Discord:
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made by Kuni <3",
+        name: "Made by Jerru <3",
         // Activity type number (0 = Playing).
         type: 2, 
       },
